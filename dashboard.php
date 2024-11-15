ca672c0607b2fee004d9c7ec68a92d3c06e266f3
kristanrey <kristanreyflores08@gmail.com>
2024-11-15 08:51:48 +0800

    added

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('db_connection.php');
+include('config.php');
 
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-include('db_connection.php');
+include('config.php');
 
 // Handle registration form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
